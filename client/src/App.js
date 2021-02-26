f3ff8420c50120180d49adc1d1e38747c641c4d5
eightys3v3n <eightys3v3n@gmail.com>
2021-02-25 18:50:21 -0700

    Working before MaterialTheme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import './App.css';
-import React from 'react';
+import React, { createElement, Component } from 'react';
 
-class LoginForm extends React.Component {
+class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -25,39 +29,39 @@ class LoginForm extends React.Component {
   render() {
     return (
       <div>
-        <header className="Login">
-          <h1>Login to my page</h1>
-        </header>
-        <section id="login_form">
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Username:
-              <input
-                type="text"
-                name="username"
-                value={this.state.value}
-                onChange={this.handleChange} />
-            </label>
-            <br />
-            <label>
-              Password:
-              <input
-                type="password"
-                name="password"
-                value={this.state.value}
-                onChange={this.handleChange} />
-            </label>
-            <br />
-            <input type="submit" value="Submit" />
-          </form>
-        </section>
+        <MuiThemeProvider>
+          <AppBar
+            title="Login" />
+          <section id="login_form">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.value}
+                  onChange={this.handleChange} />
+              </label>
+              <br />
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={this.state.value}
+                  onChange={this.handleChange} />
+              </label>
+              <br />
+              <input type="submit" value="Submit" />
+            </form>
+          </section>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
 function App() {
-  let login_form = new LoginForm();
-  return login_form.render();
+  return createElement(LoginForm);
 }
 export default App;
